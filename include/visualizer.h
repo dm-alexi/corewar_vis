7724ab261f64f02830ed203b0722ee5cf814f059
dm-alexi <dm2.alexi@gmail.com>
2020-06-18 00:25:27 +0300

    initial

---
+++ include/visualizer.h
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   visualizer.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sscarecr <sscarecr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/05/12 14:10:35 by asmall            #+#    #+#             */
+/*   Updated: 2020/06/17 22:18:20 by sscarecr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef VISUALIZER_H
+# define VISUALIZER_H
+
+# if __APPLE__
+#  include "SDL.h"
+#  include "SDL_ttf.h"
+# else
+#  include <SDL2/SDL.h>
+#  include <SDL2/SDL_ttf.h>
+# endif
+
+# include "corewar.h"
+# include "libft.h"
+
+# define SCREEN_WIDTH 1280
+# define SCREEN_HEIGHT 700
+# define SCREEN_FPS 244
+# define SCREEN_TICKS_PER_FRAME 1000 / SCREEN_FPS
+# define INFORMATION_SIZE 350
+# define WHITE (SDL_Color){255, 255, 255, 255}
+
+SDL_Window		*g_main_window;
+SDL_Renderer	*g_main_render;
+TTF_Font		*g_font;
+
+void			choose_color_pt3(t_battlefield *cell, int i);
+void			choose_color_pt2(t_battlefield *cell, int i);
+void			choose_color(t_battlefield *cell, int i);
+char			choose_color_char(int i);
+int				choose_reverse_color_pt3(t_battlefield *cell);
+int				choose_reverse_color_pt2(t_battlefield *cell);
+int				choose_reverse_color(t_battlefield *cell);
+int				init(t_vm *vm);
+
+void			push_to_render_battlefield(t_vm *vm);
+void			push_info(t_vm *vm, char *status);
+void			push_int_text(int data, char *info_text,
+					float y, SDL_Color color);
+void			push_int_slash_data(float y, int data_1,
+					int data_2, char *text);
+void			push_char_text(char *text, float y, SDL_Color color);
+void			push_live_breakdown(t_vm *vm, int y);
+void			push_pause(void);
+void			push_distribution(t_battlefield *arena, float y);
+void			push_players(t_vm *vm, int start_y);
+void			push_winner_vis(t_vm *vm);
+void			set_render_draw_color(char color);
+void			set_sdl_color(SDL_Color *color, int i);
+void			put_text_texture(float y, char *temp_2,
+					char *temp_3, SDL_Texture *text_texture);
+void			key_pause_quite(t_vm *vm);
+void			visualizer_event(t_vm *vm);
+void			live_players(t_vm *vm, SDL_FRect coor, int sum);
+
+#endif
---
+++ src/vmodule.c
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   vmodule.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sscarecr <sscarecr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/05/11 17:49:33 by asmall            #+#    #+#             */
+/*   Updated: 2020/06/17 22:26:14 by sscarecr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "corewar.h"
+#include "visualizer.h"
+
+int		arena_players_module(t_vm *vm, unsigned i, int step, int byte)
+{
+	unsigned j;
+
+	j = -1;
+	vm->start = new_process_vm(vm->start, ++vm->num_process,
+	vm->players[i].num - 1, i * step);
+	vm->players[i].amount_cursors++;
+	vm->arena[byte].cursor = 1;
+	vm->start->reg[0] = -(i + 1);
+	while (++j < vm->players[i].header.prog_size)
+	{
+		vm->arena[byte].code = vm->players[i].code[j];
+		vm->arena[byte].write_cycles = 0;
+		choose_color(&vm->arena[byte], i);
+		byte++;
+	}
+	return (byte);
+}
+
+void	init_arena_module(t_vm *vm, int byte)
+{
+	vm->arena[byte].color_b = 111;
+	vm->arena[byte].color_g = 111;
+	vm->arena[byte].color_r = 111;
+	vm->arena[byte].code = 0x0;
+	vm->arena[byte].color = 'n';
+	vm->arena[byte].write_cycles = 0;
+}
+
+void	battle_module(t_vm *vm)
+{
+	if (!vm->vis_quit)
+		push_winner_vis(vm);
+	TTF_CloseFont(g_font);
+	TTF_Quit();
+	SDL_DestroyRenderer(g_main_render);
+	SDL_DestroyWindow(g_main_window);
+	SDL_Quit();
+}
+
+void	run_pause_module(t_vm *vm, int flag)
+{
+	if (flag == 1)
+	{
+		push_to_render_battlefield(vm);
+		push_info(vm, "***Running***");
+		SDL_RenderPresent(g_main_render);
+	}
+	else
+	{
+		push_to_render_battlefield(vm);
+		push_info(vm, "***Pause***");
+		SDL_RenderPresent(g_main_render);
+	}
+}
+
+void	exec_instr_module(t_process *cur, t_vm *vm)
+{
+	vm->arena[cur->pc].cursor = 0;
+	cur->pc = cut(cur->pc + 1);
+	vm->arena[cur->pc].cursor = 1;
+}
